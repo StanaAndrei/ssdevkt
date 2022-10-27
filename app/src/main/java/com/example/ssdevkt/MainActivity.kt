5fe8144c006bdc6ce7cb73896b22d8fa11624445
Stana Andrei <andrew@Stanas-MacBook-Air.local>
2022-10-27 19:48:29 +0300

    created getcoord fn

--- app/src/main/java/com/example/ssdevkt/MainActivity.kt
+++ app/src/main/java/com/example/ssdevkt/MainActivity.kt
@@ -2,8 +2,19 @@ package com.example.ssdevkt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlin.math.floor
 
 class MainActivity : AppCompatActivity() {
+
+
+    fun getCoord(coord: Double): String {
+        val coordDeg: Int = coord.toInt()
+        val coordMins: Double = (coord - floor(coord)) * 60
+        val coordSecs: Double = (coordMins - floor(coordMins)) * 60
+        return String.format("%d%d'%.2f''", coordDeg, coordMins.toInt(), coordSecs)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
