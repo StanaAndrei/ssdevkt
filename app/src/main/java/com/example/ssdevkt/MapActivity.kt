1507512f2c5bc74f083305df4a0509a657a5d7f2
StanaAndrei <stadey33@gmail.com>
2022-10-29 21:56:14 +0300

    added maps

--- app/src/main/java/com/example/ssdevkt/MapActivity.kt
+++ app/src/main/java/com/example/ssdevkt/MapActivity.kt
@@ -1,11 +1,23 @@
 package com.example.ssdevkt
 
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import kotlin.properties.Delegates
 
 
-class MapActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    companion object {
+        private const val TAG = ".Map"
+    }
 
     private var mLat by Delegates.notNull<Double>()
     private var mLon by Delegates.notNull<Double>()
@@ -15,7 +27,20 @@ class MapActivity : AppCompatActivity() {
         setContentView(R.layout.activity_map)
         mLat = intent.getDoubleExtra("lat", .0)
         mLon = intent.getDoubleExtra("lon", .0)
+        val mapFrag = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFrag.getMapAsync(this)
+    }
 
+    override fun onMapReady(googleMap: GoogleMap) {
+        val latLng = LatLng(mLat, mLon)
+
+        val markerOption = MarkerOptions()
+        markerOption.position(latLng)
+        val markerStr = getString(R.string.marker_title)
+        markerOption.title(markerStr)
+
+        googleMap.addMarker(markerOption)
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng))
     }
 }
 
